c04728fc824b40bfd191e5bd86610c4cf1f4ff59
Beccatoni <rbccmtni1@gmail.com>
2023-11-13 16:44:52 +0200

    Update q10.js
    changed rows number and added space between stars

--- q10.js
+++ q10.js
@@ -8,9 +8,9 @@ function displayPattern(rows, columns){
     for (let i = 0; i<rows; i++){
         let row = "";
         for (let j = 0; j<columns; j++){
-            row += "*" 
+            row += "* "; 
         }
         console.log(row);
     }
 }
-displayPattern(3,7);
+displayPattern(4,3);
